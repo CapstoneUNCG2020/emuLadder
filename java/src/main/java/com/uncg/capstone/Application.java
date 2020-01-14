fda813117255256c2e8f8fb5721d4d61fdbdae56
mckenziemoize <mrmoize@uncg.edu>
2020-01-14 10:22:55 -0500

    Initial Commit

--- java/src/main/java/com/uncg/capstone/Application.java
+++ java/src/main/java/com/uncg/capstone/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CapstoneApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CapstoneApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
