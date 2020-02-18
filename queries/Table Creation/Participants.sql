2cf595045f3729781f001b103509b7fdac8af6fa
Mckenzie Moize <mrmoize@uncg.edu>
2020-02-17 21:51:50 -0500

    Update Participants.sql

--- queries/Table Creation/Participants.sql
+++ queries/Table Creation/Participants.sql
@@ -1,8 +1,11 @@
 CREATE TABLE participants (
 	email CHAR(50) NOT NULL,
 	contest_id INT NOT NULL,
+	player_id INT NOT NULL,
 	FOREIGN KEY (email)
 		REFERENCES account_information(email),
 	FOREIGN KEY (contest_id)
-		REFERENCES contest_information(contest_id)
+		REFERENCES contest_information(contest_id),
+	FOREIGN KEY (player_Id)
+		REFERENCES player_information(player_id)
 );
