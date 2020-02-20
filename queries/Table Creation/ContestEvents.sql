6a4b59d6458bbab97bcce1f50abd603bc718d3d9
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-02-20 11:44:07 -0500

    added primary key

--- queries/Table Creation/ContestEvents.sql
+++ queries/Table Creation/ContestEvents.sql
@@ -1,4 +1,6 @@
 CREATE TABLE ContestEvents (
+	contestEventsId INT AUTO_INCREMENT,
+	PRIMARY KEY (contestEventsId),
 	contestId INT,
 	FOREIGN KEY (contestId) REFERENCES Contests(contestId),
 	eventId INT,
