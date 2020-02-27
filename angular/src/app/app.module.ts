1e33b50815175853a351e45b918fb6395cfdd68d
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-02-27 13:26:58 -0500

    Added forms module for use with ngModel

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
@@ -25,7 +26,8 @@ import { DraftingPageComponent } from './drafting-page/drafting-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
