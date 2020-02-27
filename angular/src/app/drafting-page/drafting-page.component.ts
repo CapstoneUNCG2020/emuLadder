395bcd291a4245b693828d4a461e46e6557d6380
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-02-27 15:44:52 -0500

    implemented sorting of fields

--- angular/src/app/drafting-page/drafting-page.component.ts
+++ angular/src/app/drafting-page/drafting-page.component.ts
@@ -10,11 +10,18 @@ import { Game } from '../model/game';
 })
 export class DraftingPageComponent implements OnInit {
 
+  // Symbols for seeing which item is being sorted by
+  private sortSymbols = ['▲', '▼'];
+  private apSortFields = ['a-p-n', 'a-p-p', 'a-p-r', 'a-p-s'];
+  private spSortFields = ['s-p-n', 's-p-p', 's-p-r', 's-p-s'];
+
   private contest: Contest;
   private availablePlayers: Array<Player>;
   private selectedPlayers: Array<Player>;
   private countdown: string;
   private currentSalary: number;
+  private apSort: string;
+  private spSort: string;
 
   constructor() { }
 
@@ -23,7 +30,7 @@ export class DraftingPageComponent implements OnInit {
     this.resetPlayers();
 
     // Updates every second
-    setInterval(() => {this.countdown = this.getCountdown();}, 1000);
+    setInterval(() => { this.countdown = this.getCountdown(); }, 1000);
   }
 
   private testContest(): Contest {
@@ -37,7 +44,7 @@ export class DraftingPageComponent implements OnInit {
     contest.entryFee = 0;
     contest.prizeAmount = 0;
     contest.startingSalary = 40000;
-    
+
     let time = new Date();
     time.setHours(12);
     time.setMinutes(0);
@@ -96,7 +103,7 @@ export class DraftingPageComponent implements OnInit {
   getCountdown(): string {
     // Get milliseconds until contest starts
     let ms = this.contest.startTime.getTime() - new Date().getTime();
-    
+
     /* Days */
     let factor = 1000 * 60 * 60 * 24; // Factor for determining time
     let days = Math.floor(ms / factor); // Get days until contest starts
@@ -134,6 +141,7 @@ export class DraftingPageComponent implements OnInit {
       this.selectedPlayers.push(player);
 
       this.currentSalary -= player.salary;
+      if (this.spSort != undefined) this.sort(this.spSort, false);
     } else {
       // TODO: Add error message
       console.log('not enough money');
@@ -151,6 +159,11 @@ export class DraftingPageComponent implements OnInit {
     this.availablePlayers.push(player);
 
     this.currentSalary += player.salary;
+    if (this.apSort != undefined) {
+      console.log(this.apSort);
+      
+      this.sort(this.apSort, false);
+    }
   }
 
   /**
@@ -158,9 +171,124 @@ export class DraftingPageComponent implements OnInit {
    * removes all players from being selected, and resets current
    * salary back to the starting amount.
    */
-  resetPlayers() {
+  resetPlayers(): void {
     this.availablePlayers = Object.assign([], this.contest.players);
     this.selectedPlayers = new Array<Player>();
     this.currentSalary = this.contest.startingSalary;
+
+    if (this.apSort != undefined) {
+      let element = document.getElementById(this.apSort);
+
+      let curr = element.innerHTML;
+      curr = curr.slice(0, curr.length - 1);
+
+      element.innerHTML = curr;
+      this.apSort = undefined;
+    }
+
+    if (this.spSort != undefined) {
+      let element = document.getElementById(this.spSort);
+
+      let curr = element.innerHTML;
+      curr = curr.slice(0, curr.length - 1);
+
+      element.innerHTML = curr;
+      this.spSort = undefined;
+    }
+  }
+
+  /**
+   * Sorts the selected column by their ID.
+   * 
+   * @param id - the id of the column being sorted
+   * @param change - whether or not the ranking should 
+   * be swapped if selected
+   */
+  sort(id: string, change: boolean): void {
+    let element = document.getElementById(id);
+    let curr = element.innerHTML;
+    let i = curr.length - 1;
+    let sort = !change;
+
+    /* Currently being increasing sorted, 
+    now is decreasing sorted */
+    if (curr[i] == this.sortSymbols[0] && change) {
+      curr = curr.slice(0, i);
+      curr += this.sortSymbols[1];
+
+      if (id[0] == 'a') this.availablePlayers.reverse();
+      else this.selectedPlayers.reverse();
+
+      sort = false;
+    }
+
+    /* Currently being decreasing sorted, 
+    now is increasing sorted */
+    else if (curr[i] == this.sortSymbols[1] && change) {
+      curr = curr.slice(0, i);
+      curr += this.sortSymbols[0];
+
+      if (id[0] == 'a') this.availablePlayers.reverse();
+      else this.selectedPlayers.reverse();
+
+      sort = false;
+    }
+
+    /* Not currently being sorted, 
+    now is increasing sort */
+    else if (change) {
+      /* Check to see if something is already being sorted 
+      for the selected list, then unselect it */
+      if (id[0] == 'a') { // Available players
+        // Something else is already being sorted
+        if (this.apSort != undefined) {
+          // Get current value
+          let v = document.getElementById(this.apSort).innerHTML;
+
+          // Remove the sorting tag
+          v = v.slice(0, v.length - 1);
+
+          // Apply back to HTML
+          document.getElementById(this.apSort).innerHTML = v;
+        }
+
+        this.apSort = id;
+      } else { // Selected players
+        if (this.spSort != undefined) {
+          let v = document.getElementById(this.spSort).innerHTML;
+          v = v.slice(0, v.length - 1);
+          document.getElementById(this.spSort).innerHTML = v;
+        }
+
+        this.spSort = id;
+      }
+
+      curr += this.sortSymbols[0];
+      sort = true;
+    }
+
+    if (sort) {
+      /* Actually sort the elements by the desired column */
+      switch (id) {
+        case 'a-p-n': this.availablePlayers.sort((a, b) => a.name.localeCompare(b.name));
+          break;
+        case 'a-p-p': this.availablePlayers.sort((a, b) => a.position.localeCompare(b.position));
+          break;
+        case 'a-p-r': this.availablePlayers.sort((a, b) => a.rank - b.rank);
+          break;
+        case 'a-p-s': this.availablePlayers.sort((a, b) => a.salary - b.salary);
+          break;
+        case 's-p-n': this.selectedPlayers.sort((a, b) => a.name.localeCompare(b.name));
+          break;
+        case 's-p-p': this.selectedPlayers.sort((a, b) => a.position.localeCompare(b.position));
+          break;
+        case 's-p-r': this.selectedPlayers.sort((a, b) => a.rank - b.rank);
+          break;
+        case 's-p-s': this.selectedPlayers.sort((a, b) => a.salary - b.salary);
+          break;
+      }
+    }
+
+    element.innerHTML = curr;
   }
 }
