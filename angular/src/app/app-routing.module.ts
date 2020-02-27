1e98e9e8275e459cd87c95b7b6171d5049dae877
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-02-27 12:13:04 -0500

    added routes for sign in, sign up, and draft page

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { DraftingPageComponent } from './drafting-page/drafting-page.component';
 
 
 const routes: Routes = [
-  { path: 'SignUp', component: SignUpComponent}
+  { path: 'signUp', component: SignUpComponent},
+  { path: 'signIn', component: SignInComponent},
+  { path: 'drafting', component: DraftingPageComponent}
 ];
 
 @NgModule({
