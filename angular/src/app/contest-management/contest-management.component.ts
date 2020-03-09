f247332f314bdfcdcd99dd17294d88fbd717fb31
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-03-09 15:18:59 -0400

    implemented demo of edit lineup button routing to drafting page

--- angular/src/app/contest-management/contest-management.component.ts
+++ angular/src/app/contest-management/contest-management.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Contest } from '../model/contest';
 import { Player } from '../model/player';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contest-management',
@@ -11,7 +12,7 @@ export class ContestManagementComponent implements OnInit {
 
   contests: Array<Contest>;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.contests = new Array<Contest>();
@@ -83,4 +84,7 @@ export class ContestManagementComponent implements OnInit {
     return this.valueOf(sum);
   }
 
+  editContest(contest: Contest) {
+    this.router.navigateByUrl('drafting');
+  }
 }
