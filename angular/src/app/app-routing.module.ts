1f0ca603e170230f814128437589d75ea83a5a4d
Rinty <r_chowdh@uncg.edu>
2020-03-26 15:32:43 -0400

    game card

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import {GameCardComponent} from './landing-page/game-card/game-card.component';
 
 
 const routes: Routes = [
-  { path: 'SignUp', component: SignUpComponent}
+  { path: 'SignUp', component: SignUpComponent},
+  { path: 'gamecard', component: GameCardComponent}
 ];
 
 @NgModule({
