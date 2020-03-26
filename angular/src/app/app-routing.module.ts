3f0b3316ec2f8883ab7c09d01e151c0a77d4923f
Gabe Wilmoth <35712811+GabeWilmoth@users.noreply.github.com>
2020-03-26 17:20:28 -0400

    Background and Logo Animation
    Added in responsive background image for landing page, as well as animated logo for EmuLadder. Added the routing in to access the page via /landingPage

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import {GameCardComponent} from './landing-page/game-card/game-card.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 const routes: Routes = [
   { path: 'SignUp', component: SignUpComponent},
+  { path: 'landingPage', component: LandingPageComponent},
   { path: 'gamecard', component: GameCardComponent}
 ];
 
