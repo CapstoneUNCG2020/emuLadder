9816c70eff85f6b566f6e897919ec7961928f0e1
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-03-30 14:52:36 -0400

    consolidated all account management controllers into one file

---
+++ java/src/main/java/com/uncg/emuLadder/controller/AccountManagementController.java
@@ -0,0 +1,53 @@
+package com.uncg.emuLadder.controller;
+
+import com.uncg.emuLadder.handler.SignInHandler;
+import com.uncg.emuLadder.handler.SignUpHandler;
+import com.uncg.emuLadder.model.request.SignInRequestData;
+import com.uncg.emuLadder.model.request.SignUpRequestData;
+import com.uncg.emuLadder.model.response.ResponseData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/account")
+public class AccountManagementController {
+
+    private final SignInHandler signInHandler;
+    private final SignUpHandler signUpHandler;
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * Validates the user's sign in credentials (user ID and password)
+     * against the database.
+     *
+     * @param requestData - { user ID, password }
+     * @return - Boolean of true (sign in success) or false (sign in fail)
+     */
+    @PostMapping("/signIn")
+    public ResponseData<Boolean> signIn(@RequestBody SignInRequestData requestData) {
+        logger.info("Attempting sign in for {}", requestData.getUserId());
+
+        return signInHandler.handle(requestData);
+    }
+
+    /**
+     * Create a new user account stored in the database.
+     *
+     * @param requestData { user ID, first name, last name, email, phone number }
+     * @return Boolean of true (account created) or error (account could not be created)
+     */
+    @PostMapping("/signUp")
+    public ResponseData<Boolean> signUp(@RequestBody SignUpRequestData requestData) {
+        return signUpHandler.handle(requestData);
+    }
+
+    @Autowired
+    public AccountManagementController(SignInHandler signInHandler, SignUpHandler signUpHandler) {
+        this.signInHandler = signInHandler;
+        this.signUpHandler = signUpHandler;
+    }
+}
