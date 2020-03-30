59948bb4a4dcd9acd8885919dc3d23f30ea2793b
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-03-30 15:32:12 -0400

    added three needed repositories

--- java/src/main/java/com/uncg/emuLadder/service/ContestDataService.java
+++ java/src/main/java/com/uncg/emuLadder/service/ContestDataService.java
@@ -2,13 +2,32 @@ package com.uncg.emuLadder.service;
 
 import com.uncg.emuLadder.model.response.ContestData;
 import com.uncg.emuLadder.model.response.ResponseData;
+import com.uncg.emuLadder.repository.ContestPlayersRepository;
+import com.uncg.emuLadder.repository.ContestsRepository;
+import com.uncg.emuLadder.repository.PlayersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ContestDataService implements IService<Integer, ResponseData<ContestData>> {
 
+    private final ContestsRepository contestsRepository;
+    private final PlayersRepository playersRepository;
+    private final ContestPlayersRepository contestPlayersRepository;
+
     @Override
     public ResponseData<ContestData> service(Integer requestData) {
-        return null;
+        ResponseData<ContestData> responseData = new ResponseData<>();
+
+        return responseData;
+    }
+
+    @Autowired
+    public ContestDataService(final ContestsRepository contestsRepository,
+                              final PlayersRepository playersRepository,
+                              final ContestPlayersRepository contestPlayersRepository) {
+        this.contestsRepository = contestsRepository;
+        this.playersRepository = playersRepository;
+        this.contestPlayersRepository = contestPlayersRepository;
     }
 }
