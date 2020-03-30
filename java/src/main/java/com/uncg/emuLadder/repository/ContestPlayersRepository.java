30006c7a2d6fd32d4ea55edf5e03fe53c8b48b70
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-03-30 15:31:56 -0400

    fixed implementation

--- java/src/main/java/com/uncg/emuLadder/repository/ContestPlayersRepository.java
+++ java/src/main/java/com/uncg/emuLadder/repository/ContestPlayersRepository.java
@@ -1,4 +1,9 @@
 package com.uncg.emuLadder.repository;
 
-public class ContestPlayersRepository {
+import com.uncg.emuLadder.model.database.ContestPlayers;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContestPlayersRepository extends JpaRepository<ContestPlayers, Integer> {
 }
