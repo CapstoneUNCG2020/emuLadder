8f5d7353c5063b3bc91e994237f7405cc8e9a7d3
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-03-30 13:18:55 -0400

    converted to new response model

--- java/src/main/java/com/uncg/emuLadder/controller/SignInController.java
+++ java/src/main/java/com/uncg/emuLadder/controller/SignInController.java
@@ -2,7 +2,7 @@ package com.uncg.emuLadder.controller;
 
 import com.uncg.emuLadder.handler.SignInHandler;
 import com.uncg.emuLadder.model.request.SignInRequestData;
-import com.uncg.emuLadder.model.response.BooleanResponseData;
+import com.uncg.emuLadder.model.response.ResponseData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +20,7 @@ public class SignInController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @PostMapping("/signIn")
-    public BooleanResponseData signIn(@RequestBody SignInRequestData requestData) {
+    public ResponseData<Boolean> signIn(@RequestBody SignInRequestData requestData) {
         logger.info("Attempting sign in for {}", requestData.getUserId());
 
         return handler.handle(requestData);
