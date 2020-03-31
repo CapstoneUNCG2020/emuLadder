43a3704eb351bd986461242bfc066773a83f68a6
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-03-31 12:09:38 -0400

    readded app routing

---
+++ angular/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { GameCardComponent } from './landing-page/game-card/game-card.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { InviteBarComponent } from './invite-bar/invite-bar.component';
+import { DraftingPageComponent } from './drafting-page/drafting-page.component';
+import { ContestManagementComponent } from './contest-management/contest-management.component';
+import { LeagueOfLegendsPageComponent } from './league-of-legends-page/league-of-legends-page.component';
+import { CreateContestComponent } from './create-contest/create-contest.component';
+
+const routes: Routes = [
+  { path: 'account/signup', component: SignUpComponent},
+  { path: 'account/signin', component: SignInComponent},
+  { path: 'contest/invite', component: InviteBarComponent},
+  { path: 'contest/draft', component: DraftingPageComponent},
+  { path: 'contest/view', component: ContestManagementComponent},
+  { path: 'games/leagueoflegends', component: LeagueOfLegendsPageComponent},
+  { path: '', component: LandingPageComponent},
+  { path: 'gamecard', component: GameCardComponent},
+  { path: 'contest/create', component: CreateContestComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
