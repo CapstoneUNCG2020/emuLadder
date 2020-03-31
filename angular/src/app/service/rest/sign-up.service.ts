b7d1c79d0e7430ad69053c4d9949aa1389b1a0d1
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-03-31 15:39:58 -0400

    implemented service to call backend to sign up user

--- angular/src/app/service/rest/sign-up.service.ts
+++ angular/src/app/service/rest/sign-up.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { ResponseData } from 'src/app/model/response-data';
+import { RestService } from './rest.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +9,18 @@ export class SignUpService {
 
   private url = 'account/signup';
 
-  constructor() { }
+  constructor(private rest: RestService) { }
+
+  public signUp(firstName: string, lastName: string, email: string, 
+    phoneNumber: string, password: string): Promise<ResponseData<boolean>> {
+      let requestData = this.createRequestData(firstName, lastName, email, phoneNumber, password);
+
+      return this.post(requestData);
+  }
+
+  private post(requestData): Promise<ResponseData<boolean>> {
+    return this.rest.post<boolean>(this.url, requestData).toPromise();
+  }
 
   private createRequestData(firstName: string, lastName: string, email: string, 
     phoneNumber: string, password: string) {
