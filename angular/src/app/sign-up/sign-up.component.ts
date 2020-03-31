ba4ba20ce73324c877504024fde77d2192c10e62
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-03-31 15:24:06 -0400

    added basic field validation

--- angular/src/app/sign-up/sign-up.component.ts
+++ angular/src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventEmitter } from 'events';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,19 +9,61 @@ import { EventEmitter } from 'events';
 })
 export class SignUpComponent implements OnInit {
 
-  error: boolean;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  email: string;
+  password: string;
+  password2: string;
+
+  /**
+   * Whether or not the user has selected the agree to terms button.
+   */
+  agreed: boolean;
+
+  errorMessage: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  getErrorMessage(): string {
-    return 'Error creating account.';
+  signUp(): void {
+    this.errorMessage = undefined;
+
+    if (!this.verifyFieldsNotEmpty()) {
+         this.errorMessage = 'All fields are required.';
+    } else if (this.password != this.password2) {
+      this.errorMessage = 'Password do not match.';
+    } else if (!this.agreed) {
+      this.errorMessage = 'Please agree to terms';
+    } else {
+      
+    }
   }
 
-  signUp(): void {
-    this.error = !this.error;
+  verifyFieldsNotEmpty(): boolean {
+    if (this.firstName == undefined || this.firstName == '') {
+      return false;
+    }
+
+    if (this.lastName == undefined || this.lastName == '') {
+      return false;
+    }
+
+    if (this.email == undefined || this.email == '') {
+      return false;
+    }
+
+    if (this.phoneNumber == undefined || this.phoneNumber == '') {
+      return false;
+    }
+
+    if (this.password == undefined || this.password == '') {
+      return false;
+    }
+
+    return true;
   }
 
   openLogIn(): void {
