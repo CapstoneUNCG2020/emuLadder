041d85890cb34a348626b51817bf0029c60ed917
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-14 15:46:17 -0400

    added custom query for finding by start time + region

--- java/src/main/java/com/uncg/emuLadder/repository/EventsRepository.java
+++ java/src/main/java/com/uncg/emuLadder/repository/EventsRepository.java
@@ -4,6 +4,10 @@ import com.uncg.emuLadder.model.database.Events;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.util.List;
+
 @Repository
 public interface EventsRepository extends JpaRepository<Events, Integer> {
+    List<Events> findAllByStartTimeAndName(Date startTime, String name);
 }
