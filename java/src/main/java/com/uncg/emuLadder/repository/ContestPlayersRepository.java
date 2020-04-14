cd7605fa669c55792e6163ed3760ab8539960c17
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-14 14:41:49 -0400

    added custom query for finding based on contest ID and player ID

--- java/src/main/java/com/uncg/emuLadder/repository/ContestPlayersRepository.java
+++ java/src/main/java/com/uncg/emuLadder/repository/ContestPlayersRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ContestPlayersRepository extends JpaRepository<ContestPlayers, Integer> {
     List<ContestPlayers> findAllByContestId(int contestId);
-    ContestPlayers findByContestIdAndPlayerId(int contestId, String playerId);
+    Optional<ContestPlayers> findByContestIdAndPlayerId(int contestId, String playerId);
 }
