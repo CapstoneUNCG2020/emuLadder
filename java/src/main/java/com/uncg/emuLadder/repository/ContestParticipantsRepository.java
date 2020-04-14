f4ad05569d2e2176d3c7fba3a18291d3fd0db914
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-14 12:02:57 -0400

    added count to determine number of people signed up for contest

--- java/src/main/java/com/uncg/emuLadder/repository/ContestParticipantsRepository.java
+++ java/src/main/java/com/uncg/emuLadder/repository/ContestParticipantsRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContestParticipantsRepository extends JpaRepository<ContestParticipants, Integer> {
+    int countByContestId(int contestId);
 }
