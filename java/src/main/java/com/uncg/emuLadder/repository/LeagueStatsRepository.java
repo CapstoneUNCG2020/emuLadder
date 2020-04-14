1edf5d459a0588523a1b0c8bee691a126481ee59
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-14 13:54:53 -0400

    Added a method for finding information based on match ID and team ID

--- java/src/main/java/com/uncg/emuLadder/repository/LeagueStatsRepository.java
+++ java/src/main/java/com/uncg/emuLadder/repository/LeagueStatsRepository.java
@@ -5,6 +5,9 @@ import com.uncg.emuLadder.model.database.compositekeys.LeagueStatsId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LeagueStatsRepository extends JpaRepository<LeagueStats, LeagueStatsId> {
+    List<LeagueStats> findByMatchIdAndTeamId(String matchId, String teamId);
 }
