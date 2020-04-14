e17a7c83927b79904724831530b782928cf9e1cb
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-14 12:17:31 -0400

    Added method for returning all with given contest ID

--- java/src/main/java/com/uncg/emuLadder/repository/ContestPlayersRepository.java
+++ java/src/main/java/com/uncg/emuLadder/repository/ContestPlayersRepository.java
@@ -4,6 +4,9 @@ import com.uncg.emuLadder.model.database.ContestPlayers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContestPlayersRepository extends JpaRepository<ContestPlayers, Integer> {
+    List<ContestPlayers> findAllByContestId(int ContestId);
 }
