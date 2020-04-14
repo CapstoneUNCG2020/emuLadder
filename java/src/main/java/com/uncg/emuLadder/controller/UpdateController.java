ed43ad45235c94717fd8935d228d90a03130ea89
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-14 14:03:23 -0400

    Controller to perform manual updates

---
+++ java/src/main/java/com/uncg/emuLadder/controller/UpdateController.java
@@ -0,0 +1,25 @@
+package com.uncg.emuLadder.controller;
+
+import com.uncg.emuLadder.service.bl.UpdatePointsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/update")
+public class UpdateController {
+
+    private final UpdatePointsService updatePointsService;
+
+    /**
+     * Update the event points table with values from League Stats
+     */
+    @GetMapping("/points")
+    public void updatePoints() {
+        updatePointsService.updatePoints();
+    }
+
+    @Autowired
+    public UpdateController(final UpdatePointsService updatePointsService) {
+        this.updatePointsService = updatePointsService;
+    }
+}
