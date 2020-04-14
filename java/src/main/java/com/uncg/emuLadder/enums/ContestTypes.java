5c08eee474756bc23f3a2d5e4ce987d69b982084
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-14 15:25:04 -0400

    added numerical values

--- java/src/main/java/com/uncg/emuLadder/enums/ContestTypes.java
+++ java/src/main/java/com/uncg/emuLadder/enums/ContestTypes.java
@@ -1,7 +1,17 @@
 package com.uncg.emuLadder.enums;
 
 public enum ContestTypes {
-    HEAD_TO_HEAD,
-    PUBLIC,
-    PRIVATE
+    HEAD_TO_HEAD(0),
+    PUBLIC(1),
+    PRIVATE(2);
+
+    private final int value;
+
+    ContestTypes(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
