70653e6cc3c4448b0005b38844c93d9eeeabb5c6
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-16 16:44:41 -0400

    added positions

--- java/src/main/java/com/uncg/emuLadder/model/database/ContestParticipants.java
+++ java/src/main/java/com/uncg/emuLadder/model/database/ContestParticipants.java
@@ -18,6 +18,16 @@ public class ContestParticipants {
 
     private int points;
 
+    private String bottom;
+
+    private String jungle;
+
+    private String mid;
+
+    private String support;
+
+    private String top;
+
     public int getContestParticipantsId() {
         return contestParticipantsId;
     }
@@ -50,38 +60,79 @@ public class ContestParticipants {
         this.points = points;
     }
 
+    public String getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(String bottom) {
+        this.bottom = bottom;
+    }
+
+    public String getJungle() {
+        return jungle;
+    }
+
+    public void setJungle(String jungle) {
+        this.jungle = jungle;
+    }
+
+    public String getMid() {
+        return mid;
+    }
+
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+
+    public String getSupport() {
+        return support;
+    }
+
+    public void setSupport(String support) {
+        this.support = support;
+    }
+
+    public String getTop() {
+        return top;
+    }
+
+    public void setTop(String top) {
+        this.top = top;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (o == null) {
-            return false;
-        }
-
-        if (getClass() != o.getClass()) {
-            return false;
-        }
-
+        if (this == o) return true;
+        if (!(o instanceof ContestParticipants)) return false;
         ContestParticipants that = (ContestParticipants) o;
         return contestParticipantsId == that.contestParticipantsId &&
+                contestId == that.contestId &&
+                points == that.points &&
                 Objects.equals(email, that.email) &&
-                contestId == that.contestId && points == that.points;
+                Objects.equals(bottom, that.bottom) &&
+                Objects.equals(jungle, that.jungle) &&
+                Objects.equals(mid, that.mid) &&
+                Objects.equals(support, that.support) &&
+                Objects.equals(top, that.top);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(contestParticipantsId, email, contestId, points);
+        return Objects.hash(contestParticipantsId, email, contestId, points, bottom, jungle, mid, support, top);
     }
 
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("ContestParticipants{");
         sb.append("contestParticipantsId=").append(contestParticipantsId);
-        sb.append(", email=").append(email);
+        sb.append(", email='").append(email).append('\'');
         sb.append(", contestId=").append(contestId);
         sb.append(", points=").append(points);
+        sb.append(", bottom='").append(bottom).append('\'');
+        sb.append(", jungle='").append(jungle).append('\'');
+        sb.append(", mid='").append(mid).append('\'');
+        sb.append(", support='").append(support).append('\'');
+        sb.append(", top='").append(top).append('\'');
         sb.append('}');
         return sb.toString();
     }
