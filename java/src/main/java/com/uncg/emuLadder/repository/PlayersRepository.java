0750fb04a3723f06dfd7cd68039a7bfb9b23aa5f
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-16 14:16:22 -0400

    added custom query for batch lookup via player ID list

--- java/src/main/java/com/uncg/emuLadder/repository/PlayersRepository.java
+++ java/src/main/java/com/uncg/emuLadder/repository/PlayersRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface PlayersRepository extends JpaRepository<Players, String> {
     List<Players> findAllByTeamCode(String teamCode);
+    List<Players> findAllByPlayerIdIn(List<String> playerIdList);
 }
