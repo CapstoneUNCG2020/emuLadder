f69fb5b4b6b93f79660336513a5e7d270f9be902
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-16 16:49:17 -0400

    added custom query to clear drafting

--- java/src/main/java/com/uncg/emuLadder/repository/ContestParticipantsRepository.java
+++ java/src/main/java/com/uncg/emuLadder/repository/ContestParticipantsRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ContestParticipantsRepository extends JpaRepository<ContestParticipants, Integer> {
     int countByContestId(int contestId);
+    boolean deleteAllByContestIdAndEmail(int contestId, String email);
 }
