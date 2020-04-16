92a0de8b34cca8347b3953c73bb4a89a8386bb40
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-16 13:56:37 -0400

    logging

--- java/src/main/java/com/uncg/emuLadder/service/ContestDataService.java
+++ java/src/main/java/com/uncg/emuLadder/service/ContestDataService.java
@@ -1,27 +1,36 @@
 package com.uncg.emuLadder.service;
 
 import com.uncg.emuLadder.enums.ResponseStatusType;
+import com.uncg.emuLadder.model.database.Events;
 import com.uncg.emuLadder.model.response.Contest;
 import com.uncg.emuLadder.model.response.ContestData;
 import com.uncg.emuLadder.model.response.Player;
 import com.uncg.emuLadder.model.response.ResponseData;
 import com.uncg.emuLadder.service.bl.ContestDetailService;
+import com.uncg.emuLadder.service.bl.ContestEventService;
 import com.uncg.emuLadder.service.bl.PlayerDetailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class ContestDataService implements IService<Integer, ResponseData<ContestData>> {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     private final ContestDetailService contestDetailService;
     private final PlayerDetailService playerDetailService;
+    private final ContestEventService contestEventService;
 
     @Override
     public ResponseData<ContestData> service(Integer requestData) {
         ResponseData<ContestData> responseData = new ResponseData<>();
 
+        logger.info("Getting contest data for contest {}", requestData);
         Contest contest = contestDetailService.getContest(requestData);
 
         ContestData contestData = new ContestData();
@@ -36,6 +45,9 @@ public class ContestDataService implements IService<Integer, ResponseData<Contes
         Player[] players = playerDetailService.getPlayers(requestData);
         contestData.setPlayers(Arrays.asList(players));
 
+        List<Events> events = contestEventService.getNextEvents(contest.getStart(), "LCS");
+        contestData.setEvents(events);
+
         responseData.setStatus(ResponseStatusType.SUCCESS.name());
         responseData.setResponse(contestData);
 
@@ -44,8 +56,10 @@ public class ContestDataService implements IService<Integer, ResponseData<Contes
 
     @Autowired
     public ContestDataService(final ContestDetailService contestDetailService,
-                              final PlayerDetailService playerDetailService) {
+                              final PlayerDetailService playerDetailService,
+                              final ContestEventService contestEventService) {
         this.contestDetailService = contestDetailService;
         this.playerDetailService = playerDetailService;
+        this.contestEventService = contestEventService;
     }
 }
