84b0c4adc9dadbf3d34de15885bb65324671fe01
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-16 16:41:17 -0400

    sanitizer for drafting request data

---
+++ java/src/main/java/com/uncg/emuLadder/sanitizer/DraftingSanitizer.java
@@ -0,0 +1,17 @@
+package com.uncg.emuLadder.sanitizer;
+
+import com.uncg.emuLadder.model.request.CreateContestRequestData;
+import com.uncg.emuLadder.model.request.DraftingRequestData;
+import com.uncg.emuLadder.util.XSSUtil;
+import org.springframework.stereotype.Component;
+
+/**
+ * Sanitizer for drafting request data.
+ */
+@Component
+public class DraftingSanitizer implements ISanitizer<DraftingRequestData> {
+    @Override
+    public DraftingRequestData sanitize(DraftingRequestData requestData) {
+        return XSSUtil.stripXSS(requestData, CreateContestRequestData.class);
+    }
+}
