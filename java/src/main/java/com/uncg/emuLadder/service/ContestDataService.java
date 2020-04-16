084cb4abbd4e5b07e31129b396435f522dfbd843
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-16 13:05:06 -0400

    implemented

--- java/src/main/java/com/uncg/emuLadder/service/ContestDataService.java
+++ java/src/main/java/com/uncg/emuLadder/service/ContestDataService.java
@@ -1,33 +1,51 @@
 package com.uncg.emuLadder.service;
 
+import com.uncg.emuLadder.enums.ResponseStatusType;
+import com.uncg.emuLadder.model.response.Contest;
 import com.uncg.emuLadder.model.response.ContestData;
+import com.uncg.emuLadder.model.response.Player;
 import com.uncg.emuLadder.model.response.ResponseData;
-import com.uncg.emuLadder.repository.ContestPlayersRepository;
-import com.uncg.emuLadder.repository.ContestsRepository;
-import com.uncg.emuLadder.repository.PlayersRepository;
+import com.uncg.emuLadder.service.bl.ContestDetailService;
+import com.uncg.emuLadder.service.bl.PlayerDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 public class ContestDataService implements IService<Integer, ResponseData<ContestData>> {
 
-    private final ContestsRepository contestsRepository;
-    private final PlayersRepository playersRepository;
-    private final ContestPlayersRepository contestPlayersRepository;
+    private final ContestDetailService contestDetailService;
+    private final PlayerDetailService playerDetailService;
 
     @Override
     public ResponseData<ContestData> service(Integer requestData) {
         ResponseData<ContestData> responseData = new ResponseData<>();
 
+        Contest contest = contestDetailService.getContest(requestData);
+
+        ContestData contestData = new ContestData();
+        contestData.setContestType(contest.getContestType());
+        contestData.setEntreeFee(contest.getEntreeFee());
+        contestData.setName(contest.getName());
+        contestData.setPrizeAmount(contest.getPrizeAmount());
+        contestData.setStartTime(contest.getStart());
+        contestData.setRemainingSpaces(contest.getCurrentEntries());
+        contestData.setTotalSpaces(contest.getTotalEntries());
+
+        Player[] players = playerDetailService.getPlayers(requestData);
+        contestData.setPlayers(Arrays.asList(players));
+
+        responseData.setStatus(ResponseStatusType.SUCCESS.name());
+        responseData.setResponse(contestData);
+
         return responseData;
     }
 
     @Autowired
-    public ContestDataService(final ContestsRepository contestsRepository,
-                              final PlayersRepository playersRepository,
-                              final ContestPlayersRepository contestPlayersRepository) {
-        this.contestsRepository = contestsRepository;
-        this.playersRepository = playersRepository;
-        this.contestPlayersRepository = contestPlayersRepository;
+    public ContestDataService(final ContestDetailService contestDetailService,
+                              final PlayerDetailService playerDetailService) {
+        this.contestDetailService = contestDetailService;
+        this.playerDetailService = playerDetailService;
     }
 }
