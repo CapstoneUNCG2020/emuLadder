0598e18a85858934f8be46c8ce1fcaf17218f6f1
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-16 10:26:12 -0400

    added custom query for finding players by their team codes

--- java/src/main/java/com/uncg/emuLadder/repository/PlayersRepository.java
+++ java/src/main/java/com/uncg/emuLadder/repository/PlayersRepository.java
@@ -4,6 +4,9 @@ import com.uncg.emuLadder.model.database.Players;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlayersRepository extends JpaRepository<Players, String> {
+    List<Players> findAllByTeamCode(String teamCode);
 }
