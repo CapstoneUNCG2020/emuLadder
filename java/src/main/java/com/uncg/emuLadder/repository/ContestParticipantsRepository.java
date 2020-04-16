89fdb8084e9cc1cce5c5cb078077477bb76ebdbd
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-16 16:56:14 -0400

    adjusted query to have proper return value

--- java/src/main/java/com/uncg/emuLadder/repository/ContestParticipantsRepository.java
+++ java/src/main/java/com/uncg/emuLadder/repository/ContestParticipantsRepository.java
@@ -4,8 +4,12 @@ import com.uncg.emuLadder.model.database.ContestParticipants;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface ContestParticipantsRepository extends JpaRepository<ContestParticipants, Integer> {
     int countByContestId(int contestId);
-    boolean deleteAllByContestIdAndEmail(int contestId, String email);
+
+    @Transactional
+    int deleteAllByContestIdAndEmail(int contestId, String email);
 }
