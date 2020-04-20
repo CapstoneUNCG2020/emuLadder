28ebace0410f11907717374181209bf914181624
danfrye <d_frye2@uncg.edu>
2020-04-19 21:12:18 -0400

    added services and more functionality to invite-bar

--- angular/src/app/invite-bar/invite-bar.component.ts
+++ angular/src/app/invite-bar/invite-bar.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, Host } from '@angular/core';
 import { Pipe, PipeTransform} from '@angular/core';
 import { ContestManagementComponent } from '../contest-management/contest-management.component';
 import { Contest } from '../model/contest';
-//import { SendEmailService } from '../service/SendEmailService.service';
+//import { SendEmailService } from '../service/send-email.service';
+import { SendEmailService } from '../service/rest/send-email.service';
 
 @Component({
   selector: 'app-invite-bar',
@@ -11,11 +12,23 @@ import { Contest } from '../model/contest';
 })
 export class InviteBarComponent implements OnInit {
 
-  Email: String;
+  toEmail: string;
+  userEmail: string;
+  message: string;
+  subject: string;
+  error: boolean;
   
-  constructor(@Host() private parent: ContestManagementComponent) { }
-
+  constructor(@Host() private parent: ContestManagementComponent, private sendEmailService: SendEmailService) { }
+  
   ngOnInit() {
+  }
+
+  getErrorMessage(): string {
+      return 'Failed To Send'
+  }
+
+  sendEmail(): void {
+      let promise = this.sendEmailService.sendEmail(this.toEmail, this.message);
   }
 
   close() {
---
+++ angular/src/app/service/rest/send-email.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { RestService } from './rest.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SendEmailService {
+  private url = '/contest/invite';
+  constructor(private rest: RestService) { }
+
+  public sendEmail(email: string, message: string): Promise<boolean> {
+    let requestData = this.createReqData(email, message);
+    
+    return this.post(requestData);
+  }
+
+    /**
+   * Sends the request data to the backend and receives a promise back.
+   * 
+   * @param requestData { email, message }
+   */
+  private async post(requestData): Promise<boolean> {
+    let promise = await this.rest.post<boolean>(this.url, requestData).toPromise();
+
+    return promise.response;
+  }
+
+  /**
+   * request data for the backend service.
+   * 
+   * @param email 
+   * @param message
+   */
+  private createReqData(email: string, message: string) {
+    return {
+      email: email,
+      message: message
+    }
+  }
+
+
+}
