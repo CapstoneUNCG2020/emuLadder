bf810faf2ca17f1e944c2188c3a6a9d2e46dd85f
rhmeyer314 <rhmeyer314@gmail.com>
2020-04-20 14:21:51 -0400

    modified event.service.ts to take in data from backend

--- angular/src/app/service/rest/event.service.ts
+++ angular/src/app/service/rest/event.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { RestService } from './rest.service';
+import { Events } from 'src/app/model/events';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
-  constructor() { }
+  private url = '/event/test'
+
+  constructor(private rest: RestService) { } 
+
+  public getEvents(): Promise<Array<Events>> {
+    return this.get();
+  }
+
+  private async get(): Promise<Array<Events>> {
+    let promise = await this.rest.get<Array<Events>>(this.url).toPromise()
+
+    return promise.response;
+  }
 }
