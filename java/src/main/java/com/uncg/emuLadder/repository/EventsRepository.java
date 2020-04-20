ea8038345f0fa6e477ced696a4b9f71ac31fbcfb
rhmeyer314 <rhmeyer314@gmail.com>
2020-04-20 13:26:15 -0400

    modfied EventsRepository to include query to grab 5 latest events

--- java/src/main/java/com/uncg/emuLadder/repository/EventsRepository.java
+++ java/src/main/java/com/uncg/emuLadder/repository/EventsRepository.java
@@ -2,6 +2,7 @@ package com.uncg.emuLadder.repository;
 
 import com.uncg.emuLadder.model.database.Events;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
@@ -11,4 +12,7 @@ import java.util.List;
 @Repository
 public interface EventsRepository extends JpaRepository<Events, String> {
     List<Events> findAllByStartTimeAfterAndStartTimeBeforeAndName(Timestamp start, Timestamp end, String name);
+
+    @Query(value = "SELECT * FROM Events WHERE Events.name = 'LCS' AND Events.startTime > '2020-02-09 01:00:00' GROUP BY startTime ORDER BY startTime LIMIT 5", nativeQuery = true)
+    List<Events> findFiveLatestEvents();
 }
