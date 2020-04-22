39947e9cf8d1eae467754de2b757f3f7912bd7bf
rhmeyer314 <rhmeyer314@gmail.com>
2020-04-22 18:39:55 -0400

    made changes to html and ts file to display backend data

--- angular/src/app/create-contest/create-contest.component.ts
+++ angular/src/app/create-contest/create-contest.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Contest } from '../model/contest';
 import { CreateContestService } from '../service/rest/create-contest.service';
 import { EventService } from '../service/rest/event.service';
+import { Events } from 'src/app/model/events';
 
 @Component({
   selector: 'app-create-contest',
@@ -17,6 +18,9 @@ export class CreateContestComponent implements OnInit {
   name: string;
   startTime: string;
   promise: Promise<any>;
+  dateArray: Array<Date>;
+  test: Array<string>
+  dateObject: Array<object>;
 
   date = "date1"
 
@@ -29,17 +33,65 @@ export class CreateContestComponent implements OnInit {
     this.contestType = 0;
     this.opponent = 0;
     this.entryFee = 0;
+    this.dateObject = []
     this.promise = this.event.getEvents();
     this.promise.then(result => {
-      console.log(result);
-    })
-    
+      for (let res of result) {
+        let utc: Date = new Date(res);
+        let utcString = utc.toISOString();
+        console.log(utcString);
+        this.dateObject.push({
+          timestamp: new Date(utcString),
+          day: this.getDay(new Date(res)),
+          date: new Date(res).getUTCDate(),
+          time: this.getRegularTime(new Date(res)),
+        });
+      }
+      console.log(this.dateObject);
+      console.log(new Date(result[1]).getMinutes())
+    })   
+
+  }
+
+  getDay(date: Date): string {
+    let days: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    return days[date.getDay()];
+  }
+
+  getRegularTime(date: Date): string {
+    let hour: number = date.getHours();
+    let min: number = date.getMinutes();
+    let stringMin: string = "";
+    let realTimeNum: string = "";
+    let amOrPm: string = "";
+    if (hour > 11) {
+      amOrPm = "pm"
+    }
+    else {
+      amOrPm = "am";
+    }
+
+    if (min < 10) {
+      stringMin = "0" + min;
+    }
+    else {
+      stringMin = "" + min;
+    }
+
+    if (hour === 0) {
+      return "12:" + stringMin + " " + amOrPm;
+    }
+    if (hour === 12) {
+      return "12:" + stringMin + " " + amOrPm;
+    }
+
+    return hour - 12 + ":" + stringMin + " " + amOrPm;
   }
 
   selectTeam(): void {
     // Head-to-Head -> 0, Public -> 1, Private -> 2
     let contestType = this.contestType == 0 ? 0 : this.opponent == 0 ? 1 : 2;
-    
+    console.log(this.startTime);
     let promise = this.service.createContest(contestType, this.entryFee, this.name, this.startTime);
 
     promise.then(contest => {
