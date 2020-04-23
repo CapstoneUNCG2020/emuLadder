cba288262d5e8b4c69de405ea2a7d43facd70720
Gabe Wilmoth <35712811+GabeWilmoth@users.noreply.github.com>
2020-04-23 17:16:26 -0400

    Added League of Legends Page Countdown Timer
    Countdown timer will now countdown to the first contest that is displayed (works with the filters).

--- angular/src/app/league-of-legends-page/league-of-legends-page.component.ts
+++ angular/src/app/league-of-legends-page/league-of-legends-page.component.ts
@@ -17,23 +17,30 @@ export class LeagueOfLegendsPageComponent implements OnInit {
   private contests: Array<Contest>;
   private slate: Array<Schedule>;
   private contest: Contest;
+  private countdown: string;
+  private minus: number;
 
   constructor(private router: Router,
     private publicContestDetails: PublicContestDetailsService,
     private contestDetailsService: ContestDetailsService) {}
 
   ngOnInit() {
+    this.minus = 0;
     let promise = this.publicContestDetails.getPublicContestDetails();
     promise.then(contest => {
       const contestJSON = JSON.parse(JSON.stringify(contest));
       console.log(contestJSON);
       this.contests = this.DTOConversion(contestJSON);
 
-      // Updates every second
-      // setInterval(() => { this.countdown = this.getCountdown(); }, 1000);
+      //Updates every second
+      setInterval(() => { this.countdown = this.getCountdown(this.minus); }, 1000);
     });
   }
 
+  /**
+   * Handles all of the conversion between the backend and frontend display.
+   * @param contestJSON Formatted JSON from the backend
+   */
   private DTOConversion(contestJSON): Array<Contest> {
     let contests = new Array<Contest>();
     let schedules = new Array<Schedule>();
@@ -75,6 +82,10 @@ export class LeagueOfLegendsPageComponent implements OnInit {
     return contests;
   }
 
+  /**
+   * Handler for when a contest is clicked ensuring that we pass said ContestID to the drafting page.
+   * @param contestId ID of the contest that has been clicked
+   */
   selectContest(contestId): void {
     console.log(contestId);
     
@@ -85,6 +96,10 @@ export class LeagueOfLegendsPageComponent implements OnInit {
     })
   }
 
+  /**
+   * Will set a flag for which contests match the slate radio button thats been clicked. 
+   * @param clickedSlateTime The value of the slate time which has been clicked on the UI
+   */
   slateSelect(clickedSlateTime): void {
     for(let i = 0; i < this.contests.length; i++) {
       if(this.contests[i].schedules[0].slateTime == clickedSlateTime){
@@ -95,6 +110,10 @@ export class LeagueOfLegendsPageComponent implements OnInit {
     }
   }
 
+  /**
+   * Will set a flag for which contests match the style radio button thats been clicked. 
+   * @param gameStyle The value of the style which has been clicked on the UI
+   */
   styleSelect(gameStyle): void {
     if(gameStyle == 'Head2Head') {
       for(let i = 0; i < this.contests.length; i++) {
@@ -115,6 +134,9 @@ export class LeagueOfLegendsPageComponent implements OnInit {
     }
   }
 
+  /**
+   * Called as the user types in the search bar and will filter public contests by the entries
+   */
   nameSearch(): void {
     var typedValue = (<HTMLInputElement>document.getElementById("TournamentSearch")).value;
 
@@ -127,6 +149,9 @@ export class LeagueOfLegendsPageComponent implements OnInit {
     }
   }
 
+  /**
+   * Called as the user types in the Entry Fee box and will filter public contests by the fees
+   */
   entryFee(): void {
     var min = (<HTMLInputElement>document.getElementById("TextBoxInputEntryMoneyLeft")).value;
     var max = (<HTMLInputElement>document.getElementById("TextBoxInputEntryMoneyRight")).value;
@@ -154,6 +179,10 @@ export class LeagueOfLegendsPageComponent implements OnInit {
     }
   }
   
+  /**
+   * Will sort the Public Contest List by Highest or Lowest Fee's
+   * @param sortValue The value of the dropdown button selected
+   */
   sortFee(sortValue): void {
     if(sortValue == "low") {
       //Sort the public games list Low to High
@@ -164,4 +193,48 @@ export class LeagueOfLegendsPageComponent implements OnInit {
     }
   }
 
+  /**
+   * Gets the countdown until the contest starts
+   */
+  getCountdown(minus): string {
+    this.minus = minus-1;
+
+    for(let i = 0; i < this.contests.length; i++) {
+      if(this.contests[i].showFlagSlate && this.contests[i].showFlagStyle && this.contests[i].showFlagNameSearch && this.contests[i].showFlagEntryFee) {
+        // this.contests[i].startTime //get startTime and start a countdown and then break after we find the first one.
+        // Get milliseconds until contest starts
+        console.log(this.contests[i].name + ' ' + this.contests[i].schedules[0].startTime + ' ' + minus);
+        let ms = new Date(2020, 4, 8, 14, 33, 30 + minus, 0).getTime() - this.contests[i].schedules[0].startTime.getTime();
+    
+        /* Days */
+        let factor = 1000 * 60 * 60 * 24; // Factor for determining time
+        let days = Math.floor(ms / factor); // Get days until contest starts
+        ms -= days * factor; // Subtract days from total ms
+    
+        /* Hours */
+        factor /= 24;
+        let hours = Math.floor(ms / factor);
+        ms -= hours * factor;
+    
+        /* Minutes */
+        factor /= 60;
+        let minutes = Math.floor(ms / factor);
+        ms -= minutes * factor;
+    
+        /* Seconds */
+        factor /= 60;
+        let seconds = Math.floor(ms / factor);
+    
+        let timer = [days.toString(), hours.toString(), minutes.toString(), seconds.toString()];
+    
+        /* Pad hours, minutes. seconds with 0's */
+        if (hours < 10) timer[1] = '0' + timer[1];
+        if (minutes < 10) timer[2] = '0' + timer[2];
+        if (seconds < 10) timer[3] = '0' + timer[3];
+    
+        return timer.join(':');
+      }
+    }
+    return 'Countdown Error';
+  }
 }
