b6c5b00f62b17dc05efac4b5fd045cbbeb176ee3
Mckenzie Moize <mckenzie.moize@lfg.com>
2020-04-27 13:19:44 -0400

    added rank

--- java/src/main/java/com/uncg/emuLadder/model/response/RegisteredContest.java
+++ java/src/main/java/com/uncg/emuLadder/model/response/RegisteredContest.java
@@ -8,6 +8,8 @@ public class RegisteredContest {
 
     private List<Player> players;
 
+    private int rank;
+
     public Contest getContest() {
         return contest;
     }
@@ -24,25 +26,35 @@ public class RegisteredContest {
         this.players = players;
     }
 
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof RegisteredContest)) return false;
         RegisteredContest that = (RegisteredContest) o;
-        return Objects.equals(contest, that.contest) &&
+        return rank == that.rank &&
+                Objects.equals(contest, that.contest) &&
                 Objects.equals(players, that.players);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(contest, players);
+        return Objects.hash(contest, players, rank);
     }
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("RegisteredContestsData{");
+        final StringBuilder sb = new StringBuilder("RegisteredContest{");
         sb.append("contest=").append(contest);
         sb.append(", players=").append(players);
+        sb.append(", rank=").append(rank);
         sb.append('}');
         return sb.toString();
     }
